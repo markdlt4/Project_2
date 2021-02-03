bdd6a9362cdca5b83cd9210ab07595a9b57d5fd3
Ryland Quirk <quirk2580@gmail.com>
2021-02-02 19:37:49 -0600

    delete project 2 copy

--- public/js/searchResults.js
+++ public/js/searchResults.js
@@ -1,27 +1,32 @@
 //Window.location.href
-
 const apiKey = '\&api_key=oZZ6A3EEQPoNgJfv3fM7VlQt9Jcky7VyMODxwiof'
 const searchItem = 'parks'
 const searchTerm = window.location.href.replace('', '');
 
 $(document).ready(function(){
-    console.log(location.search);
+    var search = location.search
+    console.log(search);
     console.log(searchTerm);
-    event.preventDefault();
     search = 
     'https://developer.nps.gov/api/v1/' 
     + searchItem 
     + '?q=' 
     + searchTerm
     + apiKey;
-    $.ajax({
-        url: search,
-        method: 'GET'
-        }).then(function(response) {
-        document.getElementById('bodyText').innerHTML = response.data[0];
-        document.getElementById('park-name').innerHTML = response.data[0].fullName;
-        });
-    });
+    // $.ajax({
+    //     url: search,
+    //     method: 'GET'
+    //     }).then(function(response) {
+    //     document.getElementById('bodyText').innerHTML = response.data[0];
+    //     document.getElementById('park-name').innerHTML = response.data[0].fullName;
+    //     });
+    // });
+});
 
-bodyText
-park-name
+// bodyText
+// park-name
+// parks
+// places
+// campgrounds
+// events
+// parksVisitorCenter
